3a2c1ded75a6c48790775be76545d8d530ac2c32
suleymancansimsek <suleymancansimsek@gmail.com>
2022-10-18 14:56:03 +0300

    checker moving  mechanism problem solved and improved

--- src/main/java/com/example/tavla/FindObject.java
+++ src/main/java/com/example/tavla/FindObject.java
@@ -1,10 +1,7 @@
 package com.example.tavla;
 
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -14,7 +11,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Integer.compare;
 import static java.lang.Integer.parseInt;
 
 public class FindObject {
@@ -34,6 +30,7 @@ public class FindObject {
 
                     if (vBox.getBackground().getFills().get(0).getFill().toString().equals("0x90ee90ff")){
                         vBox.getChildren().add(circle);
+                        counter++;
                         turnOffGreenVboxs();
                         greenVboxs.clear();
                         try {
@@ -68,6 +65,8 @@ public class FindObject {
 //    }
     public static List<PairVboxColor> greenVboxs = new ArrayList<>();
 
+    static int counter=0;
+
     public static void getCircleObject(Pane pane) throws IOException {
         pane.setOnMousePressed(mouseEvent->{
             Object target = mouseEvent.getTarget();
@@ -75,55 +74,27 @@ public class FindObject {
                 Circle circle = (Circle) target;
                 System.out.println(circle.getId());
                 System.out.println(getVboxNumber(circle.getParent()));
-//                tempCircle = circle;
-
+                System.out.println(circle.getFill().toString());
                 PairInteger diceResults = BackgammonController.pair;
-
-                int value1 = diceResults.value1;
-                int value2 = diceResults.value2;
-                int value3 = value1 + value2;
-                int initalPosition = getVboxNumber(circle.getParent()) ;
 
 //                System.out.println(initalPosition);
                 int targetPosition1;
                 int targetPosition2;
                 int targetPosition3;
 
-
-                // for blacks
-                if (circle.getFill().toString().equals("0xfce700ff")){
-
-                    VBox targetVbox1 = BackgammonController.vBoxList.get(value1 + initalPosition - 1);
-                    VBox targetVbox2 = BackgammonController.vBoxList.get(value2 + initalPosition - 1);
-                    VBox targetVbox3 = BackgammonController.vBoxList.get(value3 + initalPosition - 1);
-
-                    String target1Color = targetVbox1.getBackground().getFills().get(0).getFill().toString();
-                    String target2Color = targetVbox2.getBackground().getFills().get(0).getFill().toString();
-                    String target3Color = targetVbox3.getBackground().getFills().get(0).getFill().toString();
-
-                    PairVboxColor vBoxColors1 = new PairVboxColor(targetVbox1,target1Color);
-                    PairVboxColor vBoxColors2 = new PairVboxColor(targetVbox2,target2Color);
-                    PairVboxColor vBoxColors3 = new PairVboxColor(targetVbox3,target3Color);
-
-                    setVboxBackgroundColorLightGreen(targetVbox1);
-                    setVboxBackgroundColorLightGreen(targetVbox2);
-                    setVboxBackgroundColorLightGreen(targetVbox3);
-
-                    for (PairVboxColor i: greenVboxs) {
-                        setVboxBackgroundColor(i.value1,i.value2);
-
+                switch (playerColor(circle)){
+                    case UNKNOWN -> {
+                        System.out.println("unknown checker error");
+                        break;
                     }
-
-
-                    greenVboxs.add(vBoxColors1);
-                    greenVboxs.add(vBoxColors2);
-                    greenVboxs.add(vBoxColors3);
-
-                    getvBoxObject(pane,circle);
-
+                    case WHITE -> {
+                        move(pane,circle,diceResults);
+                        break;}
+                    case BLACK -> {
+                        move(pane,circle,diceResults);
+                        break;
+                    }
                 }
-
-
 
             }
         });
@@ -170,11 +141,11 @@ public class FindObject {
         return allGreenVboxs;
     }
 
-    public static Players playerTurn(Circle circle){
+    public static Players playerColor(Circle circle){
         if (circle.getFill().toString().equals("0xfce700ff")){
             return Players.BLACK;
         }
-        else if(circle.getFill().toString().equals("#F637EC")){
+        else if(circle.getFill().toString().equals("0xf637ecff")){
             return Players.WHITE;
         }
         else{
@@ -182,7 +153,67 @@ public class FindObject {
         }
     }
 
+    public static void move(Pane pane, Circle circle, PairInteger diceResults){
+        int value1 = diceResults.value1;
+        int value2 = diceResults.value2;
+        int value3 = value1 + value2;
+        int initalPosition = getVboxNumber(circle.getParent()) ;
 
+        int targetPOS1 = value1 + initalPosition - 1;
+        int targetPOS2 = value2 + initalPosition - 1;
+        int targetPOS3 = value3 + initalPosition - 1;
+
+        targetPOSGenerator(targetPOS1,targetPOS2,targetPOS3);
+
+        VBox targetVbox1 = BackgammonController.vBoxList.get(targetPOS1);
+        VBox targetVbox2 = BackgammonController.vBoxList.get(targetPOS2);
+        VBox targetVbox3 = BackgammonController.vBoxList.get(targetPOS3);
+
+        String target1Color = targetVbox1.getBackground().getFills().get(0).getFill().toString();
+        String target2Color = targetVbox2.getBackground().getFills().get(0).getFill().toString();
+        String target3Color = targetVbox3.getBackground().getFills().get(0).getFill().toString();
+
+        PairVboxColor vBoxColors1 = new PairVboxColor(targetVbox1,target1Color);
+        PairVboxColor vBoxColors2 = new PairVboxColor(targetVbox2,target2Color);
+        PairVboxColor vBoxColors3 = new PairVboxColor(targetVbox3,target3Color);
+
+        setVboxBackgroundColorLightGreen(targetVbox1);
+        setVboxBackgroundColorLightGreen(targetVbox2);
+        setVboxBackgroundColorLightGreen(targetVbox3);
+
+        for (PairVboxColor i: greenVboxs) {
+            setVboxBackgroundColor(i.value1,i.value2);
+        }
+
+
+        greenVboxs.add(vBoxColors1);
+        greenVboxs.add(vBoxColors2);
+        greenVboxs.add(vBoxColors3);
+
+        getvBoxObject(pane,circle);
+    }
+
+    public static List<Integer> targetPOSGenerator(int targetPOS1, int targetPOS2, int targetPOS3){
+        List<Integer> targetPOSs = new ArrayList<>();
+        if (posControl(targetPOS1) != -1 ){
+            targetPOSs.add(targetPOS1);
+        }
+        if (posControl(targetPOS2) != -1 ){
+            targetPOSs.add(targetPOS2);
+        }
+        if (posControl(targetPOS2) != -1 ){
+            targetPOSs.add(targetPOS2);
+        }
+
+        return targetPOSs;
+    }
+
+    public static int posControl(int pos){
+        if (pos<25 && pos>0){
+            return pos;
+        }
+        return -1;
+    }
 
 }
 
---
+++ src/main/java/com/example/tavla/Player.java
@@ -0,0 +1,11 @@
+package com.example.tavla;
+
+public class Player {
+
+    boolean isPlayed;
+
+    public boolean play(){
+
+        return isPlayed;
+    }
+}
